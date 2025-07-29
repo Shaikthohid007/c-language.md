868c1d6a116745d33e597b20ac5323b66af829d0
Shaikthohid007 <shaikthohid007@gmail.com>
2025-07-29 12:49:14 +0530

    Update conditional ststement.md

--- conditional ststement.c
+++ conditional ststement.c
@@ -2,7 +2,7 @@ control statements in c:
 if ,if else,if- else ladder,nested if ,switch.
 
 1.IF STATEMENET:
-   ```c
+```c
 if (condition)
 statement;
 the condition is true ststement is executed then false then execuete another statement.
